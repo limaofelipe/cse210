7ee521a6d8813f743d41265194925ed1aecfa8d6
limaofelipe <dev.felipelima@gmail.com>
2024-06-04 09:46:28 -0300

    fundation 1

---
+++ final/Foundation1/Comment.cs
@@ -0,0 +1,22 @@
+
+
+public class Comment
+{
+    private string _commentatorName;
+    private string _theComment;
+
+    public Comment(string name, string comment)
+    {
+        _commentatorName = name;
+        _theComment = comment;
+    }
+    
+    public string GetCommentatorName()
+    {
+        return _commentatorName;
+    }
+    public string GetTheComment()
+    {
+        return _theComment;
+    }
+}
